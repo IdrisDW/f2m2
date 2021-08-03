c3110290ac3571e9cd571a1ce3e3ad5a6dfc3601
Juliet <juguquro@gmail.com>
2021-08-02 19:09:35 -0700

    ejemplo2

--- fase2m6/ejemplo-6.js
+++ fase2m6/ejemplo-6.js
@@ -1,4 +1,4 @@
-
+//CONSTRUCTORES
 
 // const john = {
 //   name: 'John',
@@ -52,7 +52,7 @@
 //   console.log( mark instanceof Number);
 
 
-
+//RETO1
 // Crear un function constructor Vec el cual representa un vector en dos dimensiones. Recibe dos parámetros x y y, 
 //ambos valores numéricos que deben ser las propiedades del objeto.
 
@@ -64,32 +64,87 @@
 
 // length: Retorna la longitud del vector, es decir, la distancia del punto (x, y) desde el origen (0, 0).
 
-const Vec = function(x, y) {
-  // Code goes here...
-  this.x = x;
-  this.y = y;
+// const Vec = function(x, y) {
+//   // Code goes here...
+//   this.x = x;
+//   this.y = y;
+// }
+// Vec.prototype.plus = function(other) {
+//   return new Vec(this.x + other.x, this.y + other.y);
+// }
+
+// Vec.prototype.minus = function(other) {
+//   return new Vec(this.x - other.x, this.y - other.y);
+// }
+
+// Vec.prototype.length = function(other) {
+//   //return  new Vec(Math.sqrt(this.x * this.x + this.y * this.y));
+//   // notquite return  new Vec(Math.sqrt(this.x * other.x + this.y * other.y));
+//   return Math.sqrt(this.x * this.x + this.y * this.y);
+// }
+
+
+// const vec1 = new Vec(1, 2);
+// const vec2 = new Vec(2, 3);
+
+// console.log(vec1.plus(vec2)); // Vec { x: 3, y: 5 }
+// console.log(vec1.minus(vec2)); // Vec { x: -1, y: -1 }
+// console.log(vec1.length()); // 2.23606797749979
+// // Para calcular la distancia desde (0, 0) hasta (x, y) se puede usar el teorema de Pitágoras: √(x2 + y2). 
+// //En JavaScript existe el método Math.sqrt para calcular raíces cuadradas.
+
+
+
+// ejemplo2
+//herencia
+
+// const Person = function(name) {
+//   this.name = name;
+// }
+
+// const john = new Person('John');
+
+
+// const Developer = function(skills, yearsOfExperience) {
+//   this.skills = skills;
+//   this.yearsOfExperience = yearsOfExperience;
+// }
+
+
+
+// const Person = function(name) {
+//   this.name = name;
+// }
+
+// const Developer = function(name, skills, yearsOfExperience) {
+//   Person.call(this, name);
+
+//   this.skills = skills;
+//   this.yearsOfExperience = yearsOfExperience;
+// }
+
+
+// const john = new Developer('John', 'JavaScript', 10);
+
+// console.log( john );
+
+
+const Person = function(name,school) {
+  this.name = name;
+  this.school = school;
 }
-Vec.prototype.plus = function(other) {
-  return new Vec(this.x + other.x, this.y + other.y);
+
+const Developer = function(name, skills, yearsOfExperience) {
+  Person.call(this, name);
+
+  this.skills = skills;
+  this.yearsOfExperience = yearsOfExperience;
 }
 
-Vec.prototype.minus = function(other) {
-  return new Vec(this.x - other.x, this.y - other.y);
-}
-
-Vec.prototype.length = function(other) {
-  //return  new Vec(Math.sqrt(this.x * this.x + this.y * this.y));
-  // notquite return  new Vec(Math.sqrt(this.x * other.x + this.y * other.y));
-  return Math.sqrt(this.x * this.x + this.y * this.y);
-}
-
-
-const vec1 = new Vec(1, 2);
-const vec2 = new Vec(2, 3);
-
-console.log(vec1.plus(vec2)); // Vec { x: 3, y: 5 }
-console.log(vec1.minus(vec2)); // Vec { x: -1, y: -1 }
-console.log(vec1.length()); // 2.23606797749979
-// Para calcular la distancia desde (0, 0) hasta (x, y) se puede usar el teorema de Pitágoras: √(x2 + y2). 
-//En JavaScript existe el método Math.sqrt para calcular raíces cuadradas.
-
+const john = new Developer('John', 'JavaScript', 10);
+const edwin = new Developer('John', 'JavaScript', 10);
+console.log( edwin);
+edwin.school = "de la vida"
+console.log(edwin);
+console.log(edwin instanceof Person);
+console.log(edwin instanceof Developer);
