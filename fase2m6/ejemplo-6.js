8c5d33aafd03e64bc806b0d327f2cbb7dda706e1
Juliet <juguquro@gmail.com>
2021-08-02 18:31:04 -0700

    reto1

--- fase2m6/ejemplo-6.js
+++ fase2m6/ejemplo-6.js
@@ -52,3 +52,44 @@
 //   console.log( mark instanceof Number);
 
 
+
+// Crear un function constructor Vec el cual representa un vector en dos dimensiones. Recibe dos parámetros x y y, 
+//ambos valores numéricos que deben ser las propiedades del objeto.
+
+// Agregar los siguientes métodos al prototype de Vec:
+
+// plus: Recibe otro vector como parámetro y retorna un nuevo vector con la suma de ambos vectores.
+
+// minus: Recibe otro vector como parámetro y retorna un nuevo vector con la diferencia de ambos vectores.
+
+// length: Retorna la longitud del vector, es decir, la distancia del punto (x, y) desde el origen (0, 0).
+
+const Vec = function(x, y) {
+  // Code goes here...
+  this.x = x;
+  this.y = y;
+}
+Vec.prototype.plus = function(other) {
+  return new Vec(this.x + other.x, this.y + other.y);
+}
+
+Vec.prototype.minus = function(other) {
+  return new Vec(this.x - other.x, this.y - other.y);
+}
+
+Vec.prototype.length = function(other) {
+  //return  new Vec(Math.sqrt(this.x * this.x + this.y * this.y));
+  // notquite return  new Vec(Math.sqrt(this.x * other.x + this.y * other.y));
+  return Math.sqrt(this.x * this.x + this.y * this.y);
+}
+
+
+const vec1 = new Vec(1, 2);
+const vec2 = new Vec(2, 3);
+
+console.log(vec1.plus(vec2)); // Vec { x: 3, y: 5 }
+console.log(vec1.minus(vec2)); // Vec { x: -1, y: -1 }
+console.log(vec1.length()); // 2.23606797749979
+// Para calcular la distancia desde (0, 0) hasta (x, y) se puede usar el teorema de Pitágoras: √(x2 + y2). 
+//En JavaScript existe el método Math.sqrt para calcular raíces cuadradas.
+
