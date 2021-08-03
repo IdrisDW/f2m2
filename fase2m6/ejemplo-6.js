aa2315213c57a0a8fea421aa0f223cd880f3f478
Juliet <juguquro@gmail.com>
2021-08-02 18:17:57 -0700

    someelse

--- fase2m6/ejemplo-6.js
+++ fase2m6/ejemplo-6.js
@@ -1,23 +1,54 @@
 
 
-const john = {
-  name: 'John',
-  birthYear: 1990,
-  job: 'Developer'
-}
+// const john = {
+//   name: 'John',
+//   birthYear: 1990,
+//   job: 'Developer'
+// }
 
 
-const Person = function(name, birthYear, job) {
-  this.name = name;
-  this.birthYear = birthYear;
-  this.job = job;
-}
+// const Person = function(name, birthYear, job) {
+//   this.name = name;
+//   this.birthYear = birthYear;
+//   this.job = job;
+// }
 
-const johnPerson = new Person('John', 1990, 'Developer');
-const johnPerson = new Person('Erandi', 1990, 'Developer');
-const johnPerson = new Person('Santiago', 1990, 'Developer');
-const johnPerson = new Person('M  
+// const johnPerson = new Person('John', 1990, 'Developer');
+// const johnPerson = new Person('Erandi', 1990, 'Developer');
+// const johnPerson = new Person('Santiago', 1990, 'Developer');
+// const johnPerson = new Person('M  
 
-', 1990, 'Developer');
+// ', 1990, 'Developer');
 
-console.log(johnPerson);
+// console.log(johnPerson);
+
+
+
+
+// const john = {
+//     name: 'John',
+//     birthYear: 1990,
+//     job: 'Developer'
+//   }
+  
+//   const Person = function(name, birthYear, job) {
+//     this.name = name;
+//     this.birthYear = birthYear;
+//     this.job = job;
+//   }
+//   //const jane = new Person('Jane', 1975, 'Designer');
+//   const Persons = [
+//       new Person('John', 1990, 'Developer');
+//     new Person('Mark', 1985, 'Teacher');
+  
+   
+  
+//   ]
+//   Persons.push(new Person('John', 1990, 'Developer'))
+   
+//   console.log(typeof mark);
+//   console.log( mark instanceof Person);
+//   console.log( mark instanceof Array);
+//   console.log( mark instanceof Number);
+
+
