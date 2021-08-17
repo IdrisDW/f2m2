20d314e1a6559f1920ebc34ad37d95e37e9a4b1b
Juliet <juguquro@gmail.com>
2021-08-16 19:48:36 -0700

    reto1

---
+++ fase2m12/ejemplo-2.js
@@ -0,0 +1,49 @@
+const app = document.getElementById('app');
+
+getGithubUsers()
+  .then(function (data) {
+
+    console.log(data);
+
+    data.forEach(function (data) {
+      app.appendChild(createCard(data));
+    })
+  })
+
+function createCard(data) {
+  const card = document.createElement('div');
+  card.className = 'card';
+
+  const name = document.createElement('h2');
+  const nameText = document.createTextNode(data.login);
+
+  name.appendChild(nameText)
+
+  const link = document.createElement('a');
+  const linkText = document.createTextNode('GitHub');
+
+  link.appendChild(linkText);
+  link.href = data.html_url;
+  link.target = 'blank';
+  link.className = 'button';
+
+  const img = document.createElement('img');
+  img.src = data.avatar_url;
+  img.alt = data.login;
+
+  card.appendChild(img);
+  card.appendChild(name);
+  card.appendChild(link);
+
+  return card;
+}
+
+function getGithubUsers() {
+  return fetch('https://api.github.com/users')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      return data.slice(0, 4)
+    })
+}
